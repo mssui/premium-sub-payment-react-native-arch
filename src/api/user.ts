b4be6f4774c114bc639672d40c216798230a301c
Asli Ari <asliari2012@gmail.com>
2026-07-28 16:51:27 +0200

    refactor(api): add api errors

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,7 +1,32 @@
-import { User } from "firebase/auth";
+import { ApiError, HttpStatus } from "@/api/ApiError";
+import { User } from "@/types/user";
 import { http } from "./httpClient";
 
 
 export function getCurrentUser() {
-    return http.get<User>("/me");
+    try {
+         http.get<User>("/me");
+         }
+catch (error) {
+
+    if (error instanceof ApiError) {
+
+        switch (error.status) {
+
+            case HttpStatus.Unauthorized:
+                // Redirect to login
+                break;
+
+            case HttpStatus.NotFound:
+                // User hasn't been created yet
+                break;
+
+            case HttpStatus.InternalServerError:
+                // Show generic error
+                break;
+        }
+
+    }
+
+}
 }
